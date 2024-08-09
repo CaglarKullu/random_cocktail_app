1f0b0a79f545e81a4792d82e15508ce5ef8eb6ce
Caglar Kullu <caglarkullu@gmail.com>
2024-08-09 17:58:32 +0100

    fix: remove statfulwidget

--- lib/widgets/screens/home_screen.dart
+++ lib/widgets/screens/home_screen.dart
@@ -1,34 +1,43 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:random_cocktail_app/widgets/screens/logged_in_widgets/logged_in_screen.dart';
 import 'package:random_cocktail_app/widgets/screens/log_in_widgets/login_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+import '../../view_models/Auth_View_Model.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: ((context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasData) {
-            return const LogedInScreen(
-              title: '',
-            );
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text("Oops something went wrong"),
-            );
-          } else {
-            return const LogInScreen();
-          }
-        }),
-      ),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authViewModelProvider);
+    log(authState.toString());
+    if (authState.isLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator.adaptive(),
+        ),
+      );
+    }
+
+    if (authState.user == null) {
+      return const LogInScreen();
+    }
+
+    if (authState.user != null) {
+      return const LogedInScreen(
+        title: '',
+      );
+    }
+
+    if (authState.error != null) {
+      return Center(
+        child: Text("Error: ${authState.error}"),
+      );
+    }
+    return const LogInScreen();
   }
 }
